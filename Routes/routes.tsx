4f0e272de9f5147416c27ad5f21b2db74c2d2fd1
AQJMTZ <aqjmtz@outlook.com>
2021-03-24 23:26:50 -0600

    Added Log-in view with other componnent like Navigation, stack, etc.

---
+++ Routes/routes.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createStacNavigation } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+interface routesProps {
+
+}
+
+export const routes: React.FC<routesProps> = ({}) => {
+        return ();
+}
