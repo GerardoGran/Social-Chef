c3a154f74216f7eea1dfe59a1415b96c6a17c225
AQJMTZ <aqjmtz@outlook.com>
2021-04-27 17:23:35 -0600

    TEST-TRYING TESTS

---
+++ Test/Fridge.test.js
@@ -0,0 +1,11 @@
+import * as React fromm 'react';
+import {Text, TouchableOpacity} from 'react-native';
+import Fridge from '../Views/Fridge'
+improt render from 'react-test-renderer';
+
+
+function Button({styles, children}){
+    retunr (
+        
+    )
+}
