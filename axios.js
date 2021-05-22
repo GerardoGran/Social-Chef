075e56c28148ca27fa5c3b3cb657629a1b7d73a9
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2021-05-21 19:37:06 -0600

    Axios for heroku

--- axios.js
+++ axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://192.168.100.22:3000' //gerardo PC
+  baseURL: 'https://social-chef-backend.herokuapp.com'
+  // baseURL: 'http://192.168.100.22:3000' //gerardo PC
 });
 
 export default instance;
